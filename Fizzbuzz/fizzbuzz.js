af350794ff5ba4a3cb24ca2a84d155b7c0024866
briacbryant <bryan2bc@dukes.jmu.edu>
2016-03-23 10:01:52 -0400

    added fizz, buzz, fuzzbuzz tests

---
+++ Fizzbuzz/fizzbuzz.js
@@ -0,0 +1,22 @@
+var count, fizzer;
+count = function(start, end) {
+	// create a variable for our output
+	var output = start;
+
+	// modify output to look like what we want
+	for (var i = start; i < end; i += 1) {
+		// code to be repeated goes here
+		output = output + " " + fizzer(i + 1);
+	}
+
+	// return the output, e.g "1 2 fuzz 4 buzz"
+	return output;
+};
+
+fizzer = function(num) {
+	return "fizz";
+};
+
+module.exports = {
+	count: count,
+	fizzer: fizzer
